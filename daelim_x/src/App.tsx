b3e784d882c4a2b70b2f34148ff0d4b24c41b7b9
raebagi <prh0208@naver.com>
2024-09-05 12:07:41 +0900

    first commit

---
+++ daelim_x/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+background-color: olive;
+width: 100vm;
+height: 100vh;
+`;
+
+function App() {
+  return (
+    <Container className="App">
+    </Container>
+  );
+}
+
+export default App;
